295cfc714c32ec574a23d412c3c2148d4c89bb4f
KevinJ3259 <kmjordan2009.kj@gmail.com>
2022-06-28 14:25:38 -0500

    initial commit

---
+++ models/comment.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    blog_post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "BlogPost",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "list",
+  }
+);
+
+module.exports = Comment;
